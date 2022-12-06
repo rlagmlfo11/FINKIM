7bc4ec1813e12dfed10162716cb0bd8f6e014f4e
rlagmlfo11 <118147005+rlagmlfo11@users.noreply.github.com>
2022-12-06 12:01:30 +0900

    Add files via upload

---
+++ 컴포넌트기본구성과 프롭스.vue
@@ -0,0 +1,30 @@
+<template>
+  <GreetingUser :username="username"></GreetingUser>
+  <GreetingUser username="awesome"></GreetingUser>
+  <GreetingUser username="FINKIM"></GreetingUser>
+  <GreetingUser username="geeks"></GreetingUser>
+  <GreetingUser></GreetingUser>
+
+  <button @click="changename">change</button>
+</template>
+
+<script>
+import GreetingUser from "./components/GreetingUser.vue";
+
+export default {
+  components: { GreetingUser },
+  data() {
+    return {
+      username: "KIMHEERAE",
+    };
+  },
+  methods: {
+    changename() {
+      this.username = "DDOCHI";
+    },
+  },
+};
+</script>
+
+<style></style>
+-->
